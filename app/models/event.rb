f3c58fabfb90918200c03163c263aa51b00a44b0
Vlad <Vlad@hotmail.co.nz>
2017-09-24 17:40:12 +1300

    setup the models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+	belongs_to :creator, :class_name => "User"
 	has_many :attendances
 	has_many :users, through: :attendances
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+	has_many :created_events, :foreign_key => "creator_id", :class_name => "Event", dependent: :destroy
 	has_many :events, through: :attendances
 end
